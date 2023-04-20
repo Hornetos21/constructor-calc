1365ed4496a73fcd88c54e748f06fa62d384c068
Igor Shapovalov <kizore5@gmail.com>
2023-04-20 17:34:15 +1000

    logic calc

--- main.js
+++ main.js
@@ -7,13 +7,54 @@ const switchContainer = document.querySelector(".switch");
 const digitsContainer = document.querySelector(".digits");
 // D&D
 const components = document.querySelectorAll(".component");
-const areaDrop = document.querySelector("[data-comp='drop']");
-const areaDrag = document.querySelector("[data-comp='drag']");
-const textArea = areaDrop.querySelector(".text-wrapper");
+const zoneDrop = document.querySelector("[data-comp='drop']");
+const zoneDrag = document.querySelector("[data-comp='drag']");
+const textArea = document.querySelector(".text-wrapper");
 
+const droppedComponents = zoneDrop.children;
 let dragComp = null;
-let droppedComp = null;
-let selectorName = "constructor";
+// test
+let bottomComp = null;
+
+function insertAboveComp(mouseY) {
+  const els = Array.from(droppedComponents);
+  let closestComp = null;
+  let closestOffset = Number.NEGATIVE_INFINITY;
+  els.forEach((comp) => {
+    const { top } = comp.getBoundingClientRect();
+    const offset = mouseY - top;
+
+    if (offset < 0 && offset > closestOffset) {
+      closestOffset = offset;
+      closestComp = comp;
+    }
+  });
+  return closestComp;
+}
+
+/* Calc Function */
+function calc(a, b, operator) {
+  let res;
+  switch (operator) {
+    case "+":
+      res = a + b;
+      break;
+    case "-":
+      res = a - b;
+      break;
+    case "*":
+      res = a * b;
+      break;
+    case "/":
+      if (!b) {
+        return (res = "Не определено");
+      }
+      res = a / b;
+
+      break;
+  }
+  return res;
+}
 
 /*Events*/
 function addAreaDropEvents(elements) {
@@ -28,6 +69,7 @@ function addAreaDropEvents(elements) {
   areaDropEvents("dragleave", dragLeave);
   areaDropEvents("drop", dragDrop);
 }
+
 function deleteAreaDropEvents(elements) {
   elements.forEach((c) => {
     c.setAttribute("draggable", "false");
@@ -43,17 +85,23 @@ function deleteAreaDropEvents(elements) {
 }
 
 function areaDropEvents(event, fn) {
-  areaDrop.addEventListener(event, fn);
-}
-function removeAreaDropEvents(event, fn) {
-  areaDrop.removeEventListener(event, fn);
+  zoneDrop.addEventListener(event, fn);
 }
 
-/* render Digits */
-arrDigits.map((item) => {
+function removeAreaDropEvents(event, fn) {
+  zoneDrop.removeEventListener(event, fn);
+}
+
+/* Render Digits */
+arrDigits.map((digit) => {
   const btnElement = document.createElement("button");
   btnElement.classList.add("btn");
-  btnElement.textContent = item;
+  btnElement.textContent = digit;
+  if (digit === ",") {
+    btnElement.value = ".";
+  } else {
+    btnElement.value = digit;
+  }
   digitsContainer.append(btnElement);
 });
 
@@ -64,13 +112,15 @@ switchContainer.addEventListener("click", ({ target }) => {
   const iconSelector = document.querySelector('.icon[name="selector"]');
   const selectorActive = target.matches(".selector");
   const iconActive = target.matches(".icon");
-  const btns = areaDrop.querySelectorAll(".btn");
-  const runComponents = Array.from(areaDrop.children);
+  const btns = zoneDrop.querySelectorAll(".btn");
+  const runComponents = Array.from(zoneDrop.children);
+
+  const input = zoneDrop.querySelector("input");
 
   if (iconActive || selectorActive) {
-    selectorName = target.parentElement.name || target.name;
+    const selectorName = target.parentElement.name || target.name;
 
-    if (selectorName === "runtime" && areaDrop.children.length < 5)
+    if (selectorName === "runtime" && zoneDrop.children.length < 4)
       return alert("Calculator is not built. Please drop all components");
 
     selectors.forEach((div) => {
@@ -79,12 +129,17 @@ switchContainer.addEventListener("click", ({ target }) => {
       if (iconActive) target.parentElement.classList.add("selector--active");
     });
 
+    let a = "";
+    let b = "";
+    let operator;
+    let equally = false;
+
     switch (selectorName) {
       case "runtime":
         iconEye.classList.remove("icon__eye");
         iconEye.classList.add("icon__eye--active");
         iconSelector.className = "icon icon__selector";
-        areaDrag.classList.add("hidden");
+        zoneDrag.classList.add("hidden");
 
         btns.forEach((btn) => btn.classList.add("btn--runtime"));
         runComponents.forEach((c) => {
@@ -92,13 +147,98 @@ switchContainer.addEventListener("click", ({ target }) => {
           if (c.dataset.item === "display") c.style.cursor = "auto";
         });
         deleteAreaDropEvents(runComponents);
+
+        // Calculator
+        zoneDrop.addEventListener("click", (e) => {
+          const parentButton = e.target.parentElement.className;
+
+          // Digits
+          if (parentButton.includes("digits")) {
+            const value = e.target.value;
+            // Validation ----------------
+            if (
+              (value === "0" && !a && !b) ||
+              (value === "." && input.value.includes("."))
+            )
+              return;
+            if (value === "." && input.value === "0") {
+              a = "0";
+              input.value = a;
+            }
+            if (!b && !operator) {
+              a += value;
+              input.value = a;
+            } else if (value === "0" && equally) {
+              console.log("Нуль");
+              b = "";
+              input.value = "0";
+            } else if (a && b && equally) {
+              b = value;
+              equally = false;
+              input.value = b;
+            } else {
+              b += value;
+              input.value = b;
+            }
+          }
+          //  Operators
+          if (parentButton.includes("operators")) {
+            operator = e.target.innerText;
+          }
+          //  Equally
+          if (parentButton.includes("equally")) {
+            console.log(e.target.innerText);
+            /*if (!sum[1] && operator) {
+							const res = calc(sum[0], sum[0], operator);
+							input.value = res;
+							equally = true;
+						}
+						if (sum.length === 2) {
+							console.log(sum);
+
+							const res = calc(sum[0], sum[1], operator);
+							input.value = res;
+							digits.length = 0;
+							sum[0] = res;
+							equally = true;
+						}*/
+
+            // Одинаковое число без нажатия по второму числу
+            if (!b && operator) {
+              b = a;
+              a = calc(parseFloat(a), parseFloat(b), operator);
+              // input.value = res;
+              // digits.length = 0;
+            }
+
+            if (b && operator) {
+              a = calc(parseFloat(a), parseFloat(b), operator);
+
+              // digits.length = 0;
+              // a = res;
+            }
+            equally = true;
+            input.value = a;
+          }
+
+          console.log("Number A: ", a, "Number B: ", b);
+          console.log("operator: ", operator);
+          console.log("=", equally);
+        });
+
         break;
       case "constructor":
         iconSelector.classList.remove("icon__selector");
         iconSelector.classList.add("icon__selector--active");
         iconEye.className = "icon icon__eye";
-        areaDrag.classList.remove("hidden");
-
+        zoneDrag.classList.remove("hidden");
+        /*Reset Calc*/
+        a = "";
+        b = "";
+        operator = "";
+        equally = false;
+        input.value = "0";
+        /*-----------------*/
         btns.forEach((btn) => btn.classList.remove("btn--runtime"));
         addAreaDropEvents(runComponents);
 
@@ -114,6 +254,7 @@ switchContainer.addEventListener("click", ({ target }) => {
     }
   }
 });
+
 /* Double Click*/
 function doubleClick({ target }) {
   const arr = Array.from(components);
@@ -127,17 +268,17 @@ function doubleClick({ target }) {
   shadowElement[0].classList.remove("cursor--auto");
   shadowElement[0].setAttribute("draggable", "true");
 
-  if (areaDrop.children.length === 1) textArea.classList.remove("hidden");
+  if (!zoneDrop.children.length) textArea.classList.remove("hidden");
 }
 
 //* Drag start*/
-function dragStart(e) {
+function dragStart({ target }) {
   console.log("drag start");
 
   dragComp = this;
 
-  if (e.target.parentElement.dataset.comp === "drag") {
-    dragComp = e.target.cloneNode(true);
+  if (target.parentElement.dataset.comp === "drag") {
+    dragComp = target.cloneNode(true);
     dragComp.classList.remove("shadow");
 
     dragComp.addEventListener("dragstart", dragStart);
@@ -162,46 +303,65 @@ function dragOver(e) {
   e.preventDefault();
   e.stopPropagation();
   e.dataTransfer.effectAllowed = "move";
-  const droppedComps = areaDrop.children;
 
   textArea.style.background = "#F0F9FF";
 
-  if (droppedComps.length > 1) {
-    droppedComps[droppedComps.length - 1].classList.add("highlight-line-after");
+  // Test
+  if (droppedComponents.length) {
+    bottomComp = insertAboveComp(e.clientY);
+    bottomComp?.classList.add("highlight-line-before");
+  }
+
+  //----test
+
+  if (droppedComponents.length) {
+    // droppedComponents[droppedComponents.length - 1].classList.add(
+    //   "highlight-line-after"
+    // );
     /*if (e.target.matches(".component")) {
-      e.target.classList.add("highlight-line-before");
-      if (!e.target.matches(".component")) {
-        droppedComps[droppedComps.length - 1].classList.remove(
-          "highlight-line-before"
-        );
-      }
-    }*/
+			e.target.classList.add("highlight-line-before");
+			if (!e.target.matches(".component")) {
+				droppedComps[droppedComps.length - 1].classList.remove(
+					"highlight-line-before"
+				);
+			}
+		}*/
   }
 }
+
 /* Drag left*/
 function dragLeave() {
   console.log("drag left");
   textArea.style.background = "#FFF";
-  areaDrop.children[areaDrop.children.length - 1].classList.remove(
-    "highlight-line-after"
-  );
+  if (droppedComponents.length) {
+    bottomComp?.classList.remove("highlight-line-before");
+
+    // droppedComponents[droppedComponents.length - 1].classList.remove(
+    //   "highlight-line-after"
+    // );
+  }
 }
+
 /* Drag dropped*/
 function dragDrop(e) {
   console.log("drag dropped");
-  areaDrop.children[areaDrop.children.length - 1].classList.remove(
-    "highlight-line-after"
-  );
-  droppedComp = dragComp;
-  // if (areaDrop.children.length === 1)
-  this.append(dragComp);
-  console.log(e.target);
-  /*  if(){
-    this.before(dragComp)
-  }else {
+  if (droppedComponents.length) {
+    bottomComp?.classList.remove("highlight-line-before");
+    // droppedComponents[droppedComponents.length - 1].classList.remove(
+    //   "highlight-line-after"
+    // );
+  }
 
-  }*/
+  /*Test*/
+  if (!bottomComp) {
+    this.appendChild(dragComp);
+  } else {
+    this.insertBefore(dragComp, bottomComp);
+  }
+
+  /*-----*/
 }
+
 /* Drag End*/
 function dragEnd(e) {
   console.log("drag end");
@@ -220,18 +380,5 @@ function dragEnd(e) {
 
   textArea.classList.add("hidden");
 }
-if (selectorName === "constructor") addAreaDropEvents(components);
 
-/* Calc Functions */
-function calc(num1, num2, operator) {
-  switch (operator) {
-    case "+":
-      return num1 + num2;
-    case "-":
-      return num1 - num2;
-    case "*":
-      return num1 * num2;
-    case "/":
-      return num1 / num2;
-  }
-}
+if (!droppedComponents.length) addAreaDropEvents(components);
